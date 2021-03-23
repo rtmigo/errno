b95f9a1d9bf273a8d48bbbabe919bca9af645cb9
rtmigo <art.galkin@gmail.com>
2021-03-23 06:15:11 +0300

    initial

--- lib/errno.dart
+++ lib/errno.dart
@@ -1,4 +1,6 @@
+// SPDX-FileCopyrightText: (c) 2021 Art Galkin <github.com/rtmigo>
+// SPDX-License-Identifier: BSD-3-Clause
 
-export 'src/errno_h_linux.dart';
-export 'src/errno_h_darwin_cfull_unix03.dart';
-export 'src/errno_h_windows.dart';
+export 'src/errno_h_linux.dart' show LinuxErrno;
+export 'src/errno_h_darwin_cfull_unix03.dart' show DarwinErrno;
+export 'src/errno_h_windows.dart' show WindowsErrno;
